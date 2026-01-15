69392e51337f0af976e06fdf3cacf60bb86bffa8
Prasanth Guru <18697-guruprasanth17102003@users.noreply.git.selfmade.ninja>
2026-01-15 09:05:47 +0000

    template ready

---
+++ libs/load.php
@@ -0,0 +1,13 @@
+<?php
+function load_template($name)
+{
+    include $_SERVER['DOCUMENT_ROOT']."/app/_templates/$name.php"; //not consistant.
+}
+function validate_credentials($username, $password)
+{
+    if ($username == "sibi@selfmade.ninja" and $password == "password") {
+        return true;
+    } else {
+        return false;
+    }
+}
