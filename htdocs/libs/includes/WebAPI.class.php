a667330acae42663844615814a6aab94eabc27c5
Prasanth Guru <18697-guruprasanth17102003@users.noreply.git.selfmade.ninja>
2026-01-26 10:26:49 +0000

    dir fix

--- htdocs/libs/includes/WebAPI.class.php
+++ htdocs/libs/includes/WebAPI.class.php
@@ -4,14 +4,17 @@ class WebAPI
 {
     public function __construct()
     {
+        // global $__site_config;
+        // if (php_sapi_name() == "cli") {
+        //     $__site_config_path = "/home/guruprasanth17102003/photogramconfig.json";
+        //     $__site_config = file_get_contents($__site_config_path);
+        // } else {
+        //     $__site_config_path = dirname(is_link($_SERVER['DOCUMENT_ROOT']) ? readlink($_SERVER['DOCUMENT_ROOT']) : $_SERVER['DOCUMENT_ROOT']).'/photogramconfig.json';
+        //     $__site_config = file_get_contents($__site_config_path);
+        // }
         global $__site_config;
-        if (php_sapi_name() == "cli") {
-            $__site_config_path = "/home/guruprasanth17102003/photogramconfig.json";
-            $__site_config = file_get_contents($__site_config_path);
-        } else {
-            $__site_config_path = dirname(is_link($_SERVER['DOCUMENT_ROOT']) ? readlink($_SERVER['DOCUMENT_ROOT']) : $_SERVER['DOCUMENT_ROOT']).'/photogramconfig.json';
-            $__site_config = file_get_contents($__site_config_path);
-        }
+        $__site_config_path = __DIR__.'/../../../project/photogramconfig.json';
+        $__site_config = file_get_contents($__site_config_path);
         Database::getConnection();
     }
 
