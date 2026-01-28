16605137878ddd5b732be3ef1127fa8d8ecca876
Prasanth Guru <18697-guruprasanth17102003@users.noreply.git.selfmade.ninja>
2026-01-28 16:07:57 +0000

    libs restructure

--- htdocs/libs/includes/Session.class.php
+++ htdocs/libs/includes/Session.class.php
@@ -1,7 +1,12 @@
 <?php
 
+use MongoDB\Driver\Session as DriverSession;
+
 class Session
 {
+    public static $isError = false;
+    public static $user = null;
+    public static $usersession = null;
     public static function start()
     {
         session_start();
@@ -40,6 +45,52 @@ class Session
             return $_SESSION[$key];
         } else {
             return $default;
+        }
+    }
+
+    public static function getUser()
+    {
+        return Session::$user;
+    }
+
+    public static function getUserSession(){
+        return Session::$usersession;
+    }
+
+
+    public static function loadTemplate($name)
+    {
+        $script = $_SERVER['DOCUMENT_ROOT'] . get_config('base_path'). "_templates/$name.php";
+        if (is_file($script)) {
+            include $script;
+        } else {
+            Session::loadTemplate('_error');
+        }
+    }
+
+    public static function renderPage()
+    {
+        Session::loadTemplate('_master');
+    }
+
+    public static function currentScript()
+    {
+        return basename($_SERVER['SCRIPT_NAME'], '.php');
+    }
+
+    public static function isAuthenticated()
+    {
+        //TODO: Is it a correct implementation?
+        if(is_object(Session::getUserSession())){
+            return Session::getUserSession()->isValid();
+        } 
+        return false;
+    }
+
+    public static function ensureLogin(){
+        if(!Session::isAuthenticated()){
+            header("Location: /login.php");
+            die();
         }
     }
 }
--- htdocs/libs/includes/WebAPI.class.php
+++ htdocs/libs/includes/WebAPI.class.php
@@ -4,14 +4,6 @@ class WebAPI
 {
     public function __construct()
     {
-        // global $__site_config;
-        // if (php_sapi_name() == "cli") {
-        //     $__site_config_path = "/home/guruprasanth17102003/photogramconfig.json";
-        //     $__site_config = file_get_contents($__site_config_path);
-        // } else {
-        //     $__site_config_path = dirname(is_link($_SERVER['DOCUMENT_ROOT']) ? readlink($_SERVER['DOCUMENT_ROOT']) : $_SERVER['DOCUMENT_ROOT']).'/photogramconfig.json';
-        //     $__site_config = file_get_contents($__site_config_path);
-        // }
         global $__site_config;
         $__site_config_path = __DIR__.'/../../../project/photogramconfig.json';
         $__site_config = file_get_contents($__site_config_path);
@@ -20,8 +12,14 @@ class WebAPI
 
     public function initiateSession()
     {
-        if (session_status() !== PHP_SESSION_ACTIVE) {
-            Session::start();
+        Session::start();
+        if (Session::isset("session_token")) {
+            try {
+                Session::$usersession = UserSession::authorize(Session::get('session_token')); 
+            } 
+            catch (Exception $e){
+                //TODO: Handle error
+            }
         }
     }
 }
